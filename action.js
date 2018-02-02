8dc5d67cfd67f77ea10bf52a35b837ce6df1bb4d
Jerliyah <jerliyahcraig@gmail.com>
2018-02-02 02:17:45 -0600

    filters!

--- action.js
+++ action.js
@@ -4,16 +4,59 @@ const player = document.querySelector('video');
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-const snap_btn = document.querySelector('button#snap');
 const snaps_ctn = document.querySelector('div#snaps-ctn');
+const snap_btn = document.querySelector('button#snap');
+const filters = Array.from( document.querySelectorAll('div#filters > button') );
+
 
 
 /* ====== Variables ====== */
 var snap_count = 1;
+var painting;
 
 
 
 /* ====== Functions ====== */
+function filter_red(pixels) {
+    for(let i=0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;  // green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+    }
+
+    return pixels
+}
+
+function filter_green(pixels) {
+    for(let i=0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] * 0.5; // red
+        pixels.data[i + 1] = pixels.data[i + 1] + 100; // green
+        pixels.data[i + 2] = pixels.data[i + 2] - 50;  // blue
+    }
+
+    return pixels
+}
+
+function filter_blue(pixels) {
+    for(let i=0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] - 50;   // red
+        pixels.data[i + 1] = pixels.data[i + 1] * 0.5;  // green
+        pixels.data[i + 2] = pixels.data[i + 2] + 100;  // blue
+    }
+
+    return pixels
+}
+
+function filter_surreal(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // red
+        pixels.data[i + 100] = pixels.data[i + 1]; // green
+        pixels.data[i - 150] = pixels.data[i + 2]; // blue
+      }
+    return pixels;
+}
+
+
 function get_video() {
     navigator.mediaDevices.getUserMedia({video:true, audio:false})
     .then( (media_obj) => {
@@ -27,15 +70,40 @@ function get_video() {
 
 
 function paint_canvas() {
+    clearInterval(painting)
+
 
     canvas.width = player.videoWidth;
     canvas.height = player.videoHeight;
 
-    setInterval( () => {
+    let filter = this.dataset.filter || null;
+
+    painting = setInterval( () => {
         context.drawImage(player, 0, 0, canvas.width, canvas.height)
+
+        if( filter ) {
+            let pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+
+            var filtered_pixels = (() => {
+                // Would be nice if this could be dynamic function calls
+                switch( filter ) {
+                    case 'red':     return filter_red(pixels)
+                    case 'green':   return filter_green(pixels)
+                    case 'blue':    return filter_blue(pixels)
+                    case 'surreal': return filter_surreal(pixels)
+                    default: return pixels
+                }
+            })()
+
+            context.putImageData(filtered_pixels, 0, 0);
+            // console.log(filtered_pixels)
+        }
+
     }, 
     16 /* Loop every 16 milliseconds */);
 }
+
+
 
 function take_snapshot() {
     let img_data_url = canvas.toDataURL('image/jpeg');
@@ -56,9 +124,12 @@ function take_snapshot() {
 
 
 
+
 /* ====== Events ====== */
 get_video()
 player.addEventListener('canplay', paint_canvas)
 snap_btn.addEventListener('click', take_snapshot)
+
+filters.forEach( (filter) => filter.addEventListener('click', paint_canvas) )
 
 
